8ad1a12b700571e4ac50229eadbc368395bda07d
PabloRuiz <pabloruiz1992@outlook.com.ar>
2023-04-14 15:30:12 -0300

    Basci Js implemented

--- js/javaScript.js
+++ js/javaScript.js
@@ -1,6 +1,31 @@
+let clickScore1=document.getElementById("btnScore1");
+let clickScore2=document.getElementById("btnScore2");
+let clickScore3=document.getElementById("btnScore3");
+let clickScore4=document.getElementById("btnScore4");
+let clickScore5=document.getElementById("btnScore5");
 let clickSubmit=document.getElementById("btnSubmit");
 
 clickSubmit.addEventListener("click",()=>{
     document.getElementById("containerThankYou").style.visibility="visible";
     document.getElementById("containerRating").style.visibility="hidden";
+});
+
+clickScore1.addEventListener("click",()=>{
+    document.getElementById("selectedScore").innerText = document.getElementById("btnScore1").value;
+});
+
+clickScore2.addEventListener("click",()=>{
+    document.getElementById("selectedScore").innerText = document.getElementById("btnScore2").value;
+});
+
+clickScore3.addEventListener("click",()=>{
+    document.getElementById("selectedScore").innerText = document.getElementById("btnScore3").value;
+});
+
+clickScore4.addEventListener("click",()=>{
+    document.getElementById("selectedScore").innerText = document.getElementById("btnScore4").value;
+});
+
+clickScore5.addEventListener("click",()=>{
+    document.getElementById("selectedScore").innerText = document.getElementById("btnScore5").value;
 });
